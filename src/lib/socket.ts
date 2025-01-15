bfed90d632aa8f0245517caca94a50b2bf610be6
Your Name <sunilcrestha9@gmai.com>
2025-01-15 08:08:53 +0545

    railway url

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import { Socket } from "socket.io-client";
 
-const socket: typeof Socket = io("https://sajiloride.vercel.app/", {
+const socket: typeof Socket = io("https://sajiloride-production.up.railway.app/", {
   transports: ["websocket"],
 });
 
