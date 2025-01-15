d110515f93757d01c21f7de96129f6dd8c1eddc3
Your Name <sunilcrestha9@gmai.com>
2025-01-15 07:59:05 +0545

    some new changes

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import { Socket } from "socket.io-client";
 
-const socket: typeof Socket = io("http://localhost:4000", {
+const socket: typeof Socket = io("https://sajiloride.vercel.app/", {
   transports: ["websocket"],
 });
 
