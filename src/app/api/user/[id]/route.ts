3c9affba1024230a712dd50f39cf00b3d8aa478f
Your Name <sunilcrestha9@gmai.com>
2025-01-25 16:14:05 +0545

    params errror solved

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,46 +1,43 @@
+import { NextResponse } from "next/server";
 import dbConnect from "@/lib/db";
 import User from "@/models/user.model";
-import { NextResponse } from "next/server";
 
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
-   const _id = params.id;
-   await dbConnect();
+export async function PUT(request: Request, { params }: {params :Promise<{ id: string }>} ) {
+  const {id:_id} = await params
+  await dbConnect();
 
-   try {
-      const validUser = await User.findById(_id);
-      if (!validUser) {
-         return NextResponse.json(
-            { success: false, message: "User not found" },
-            { status: 404 }
-         );
-      }
-
-      const { name, email, password, avatar,phone,vehicle } = await request.json();
-
-      const updatedUser = await User.findByIdAndUpdate(
-         _id,
-         {
-            $set: {
-               name,
-               email,
-               password,
-               avatar,
-               phone,
-               vehicle
-            },
-         },
-         { new: true } // This option returns the updated document
-      );
-
+  try {
+    const validUser = await User.findById(_id);
+    if (!validUser) {
       return NextResponse.json(
-         { success: true, message: "User updated successfully", user: updatedUser },
-         { status: 200 }
+        { success: false, message: "User not found" },
+        { status: 404 }
       );
-   } catch (error) {
-      return NextResponse.json(
-         { success: false, message: "Error updating user" },
-         { status: 500 }
-      );
-   }
+    }
 
+    const { name, email, password, avatar } = await request.json();
+
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        $set: {
+          name,
+          email,
+          password,
+          avatar,
+        },
+      },
+      { new: true } // This option returns the updated document
+    );
+
+    return NextResponse.json(
+      { success: true, message: "User updated successfully", user: updatedUser },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: "Error updating user" },
+      { status: 500 }
+    );
+  }
 }
