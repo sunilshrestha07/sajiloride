a329a97fafbc48b741b57c2eb15c9b8e81bb331e
Your Name <sunilcrestha9@gmai.com>
2025-01-25 17:00:34 +0545

    cange pw

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,24 +1,21 @@
-import { NextResponse } from "next/server";
-import dbConnect from "@/lib/db";
-import User from "@/models/user.model";
+import {NextResponse} from 'next/server';
+import dbConnect from '@/lib/db';
+import User from '@/models/user.model';
 import bcrypt from 'bcryptjs';
 
-export async function PUT(request: Request, { params }: {params :Promise<{ id: string }>} ) {
-  const {id:_id} = await params
+export async function PUT(request: Request, {params}: {params: Promise<{id: string}>}) {
+  const {id: _id} = await params;
   await dbConnect();
 
   try {
     const validUser = await User.findById(_id);
     if (!validUser) {
-      return NextResponse.json(
-        { success: false, message: "User not found" },
-        { status: 404 }
-      );
+      return NextResponse.json({success: false, message: 'User not found'}, {status: 404});
     }
 
-    const { name, email, password, avatar,phone,vehicle } = await request.json();
+    const {name, email, password, avatar, phone, vehicle} = await request.json();
 
-    if(password){
+    if (password) {
       const hashedpassword = bcrypt.hashSync(password, 10);
       const updatedUser = await User.findByIdAndUpdate(
         _id,
@@ -26,39 +23,33 @@ export async function PUT(request: Request, { params }: {params :Promise<{ id: s
           $set: {
             name,
             email,
-            password:hashedpassword,
+            password: hashedpassword,
             avatar,
             phone,
-            vehicle
+            vehicle,
           },
         },
-        { new: true } // This option returns the updated document
+        {new: true} // This option returns the updated document
       );
-    }
-
-    const updatedUser = await User.findByIdAndUpdate(
-      _id,
-      {
-        $set: {
-          name,
-          email,
-          password,
-          avatar,
-          phone,
-          vehicle
+      return NextResponse.json({success: true, message: 'User updated successfully', user: updatedUser}, {status: 200});
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            name,
+            email,
+            password,
+            avatar,
+            phone,
+            vehicle,
+          },
         },
-      },
-      { new: true } // This option returns the updated document
-    );
-
-    return NextResponse.json(
-      { success: true, message: "User updated successfully", user: updatedUser },
-      { status: 200 }
-    );
+        {new: true} // This option returns the updated document
+      );
+      return NextResponse.json({success: true, message: 'User updated successfully', user: updatedUser}, {status: 200});
+    }
   } catch (error) {
-    return NextResponse.json(
-      { success: false, message: "Error updating user" },
-      { status: 500 }
-    );
+    return NextResponse.json({success: false, message: 'Error updating user'}, {status: 500});
   }
 }
