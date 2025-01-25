d06171fb1122d1eb59ffb5efbbe20f5597b72565
Your Name <sunilcrestha9@gmai.com>
2025-01-25 16:56:32 +0545

    changes n ow

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/db";
 import User from "@/models/user.model";
+import bcrypt from 'bcryptjs';
 
 export async function PUT(request: Request, { params }: {params :Promise<{ id: string }>} ) {
   const {id:_id} = await params
@@ -16,6 +17,24 @@ export async function PUT(request: Request, { params }: {params :Promise<{ id: s
     }
 
     const { name, email, password, avatar,phone,vehicle } = await request.json();
+
+    if(password){
+      const hashedpassword = bcrypt.hashSync(password, 10);
+      const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        {
+          $set: {
+            name,
+            email,
+            password:hashedpassword,
+            avatar,
+            phone,
+            vehicle
+          },
+        },
+        { new: true } // This option returns the updated document
+      );
+    }
 
     const updatedUser = await User.findByIdAndUpdate(
       _id,
