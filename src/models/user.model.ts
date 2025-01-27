aac52e74e45f64954caf382339b445cf5b16bc67
Your Name <sunilcrestha9@gmai.com>
2025-01-27 18:01:08 +0545

    some changes

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -38,17 +38,14 @@ const userSchema = new mongoose.Schema({
   vehicle: {
     color: {
       type: String,
-      required: true,
       minlength: [3, 'Color must be at least 3 characters long'],
     },
     plate: {
       type: String,
-      required: true,
       minlength: [3, 'Plate must be at least 3 characters long'],
     },
     vehicleType: {
       type: String,
-      required: true,
     },
   },
 });
